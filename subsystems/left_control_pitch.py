5f1686dafc27413ee60c7accad9474f65a844f1b
Reyna-5970 <reyna.j.ayala@gmail.com>
2020-03-26 02:16:05 +0000

    acquired basic control of servo. next step will be zeroing position initial

--- subsystems/left_control_pitch.py
+++ subsystems/left_control_pitch.py
@@ -1,5 +1,6 @@
 # libraries
 import RPi.GPIO as GPIO
+import time
 
 class Left_Control_Pitch():
 
@@ -11,17 +12,24 @@ class Left_Control_Pitch():
 
         ### VARIABLES ###
         p = None
+        self.is_ready = False
+        self.old_time = time.time()
 
         ### PREPARE PWM ###
         servo_pin = 17
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(servo_pin, GPIO.OUT)
         self.p = p
-        self.p = GPIO.PWM(servo_pin, 50) #arbitrary Hz
-        self.p.start(2.5)
+        self.p = GPIO.PWM(servo_pin, 100) #arbitrary Hz
+
+        self.p.start(0.3)
+        time.sleep(0.15)
+        self.p.ChangeDutyCycle(0)
+        time.sleep(1)
 
     def level(self):
-        self.p.ChangeDutyCycle(5)
+        self.p.ChangeDutyCycle(1)
+        time.sleep(0.15)
 
     def up():
         pass
