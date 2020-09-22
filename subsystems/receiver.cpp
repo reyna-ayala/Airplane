65b2307e6464858875d02fcabce44f8c132390c9
Reyna-5970 <reyna.j.ayala@gmail.com>
2020-09-22 09:12:47 -0700

    created CPP code on raspberry pi to match first trial of RF transmission from Arduino. Lighting LED.

--- subsystems/receiver.cpp
+++ subsystems/receiver.cpp
@@ -3,23 +3,48 @@
 # include <nRF24L01.h>
 # include <RF24.h>
 
+// def radio
 RF24 radio(7, 8); //CE, CSN
 
+// user config
+bool radioNumber = 0;
+bool role = 0;
+
+// constants
 const byte address[6] = "00001"
 
+// define own data types
+struct rpiTransmit{
+	bool resultLEDstatus;
+};
+
 void setup() {
-	//Serial.begin(9600);
+	Serial.begin(9600);
+	Serial.println(F("*** PRESS 'T' to begin transmitting to arduino node"));
 	radio.begin();
-	radio.openReadingPipe(0, address);
-	radio.setPALevel(RF24_PA_MIN);
+	radio.setPALevel(RF24_PA_LOW);
+
+	if (radioNumber){
+		radio.openWritingPipe(addresses[1]);
+		radio.openReadingPipe(1, addresses[0]);
+	}else{
+		radio.openWritingPipe(addresses[0]);
+		radio.openReadingPipe(1, addresses[1]);
+	}
+
 	radio.startListening();
 }
 
 void loop() {
-	if (radio.available()) {
-		char text[32] = "";
-		radio.read(&text, sizeof(text));
-	//	Serial.println(text);
-		print(text)
+	if (role == 0){
+		if (radio.available()){
+			while (radio.available()){
+				radio.read(&transmit, sizeof(transmit));
+			}
+		}
+		radio.stopListening();
+		radio.write(&rpiTransmit, sizeof(rpiTransmit));
+		radio.startListening();
+		Serial.print(F("Sent response");
 	}
 }
